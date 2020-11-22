834b6e6acd7b64ba00497e9d7d7d5556807f526d
Luc Bertin <lucbertin@outlook.com>
2020-11-22 21:05:37 +0100

    corrected typo deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 # create remotely the app folder
 ssh -p 22 administrateur@devincidocker01003.northeurope.cloudapp.azure.com 'mkdir app'
 # copy files from app folder to the remote app folder
-scp -r ./app/* administrateur@devincidocker01003.northeurope.cloudapp.azure.com:~/app/*
+scp -r ./app/* administrateur@devincidocker01003.northeurope.cloudapp.azure.com:~/app/
 # copy the docker-compose file to the remote
 scp docker-compose.yml administrateur@devincidocker01003.northeurope.cloudapp.azure.com:~/
 # docker-compose up on the remote 
