6be03ee2e3d2b813fc073385051a2f86c7351404
Luc Bertin <lucbertin@outlook.com>
2020-11-22 17:06:02 +0100

    first commit

---
+++ app/flaskapp.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from flask import Flask
+from redis import Redis, RedisError
+
+import os
+import socket
+redis = Redis(host="redis-server", db=0, socket_connect_timeout=10, socket_timeout=2)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</h1>"
+
+@app.route("/visit")
+def hello():
+	try:
+		visits = redis.incr("counter")
+	except RedisError:
+		visits = "<i>cannot connect to Redis server to count</i>"
+	html = "<h3>Hello World!</h3>\n" \
+		   "<b>Hostname:</b> {hostname}<br/>\n" \
+		   "<b>Visits:</b> {visits}\n"
+	return html.format(hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80)
+
+
+
+
+
+
+
+
+# docker run --name myredisdb -d redis
+# docker run --name flaskapap -P --link myredisdb:redis-server 8eb22f5deaf8
---
+++ app/test_basics.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from flaskapp import app
+from redis import Redis
+
+
+class CounterTest(unittest.TestCase):
+    ### step up and teardown ###
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+    
+
+
+    # actual tests #
+    def test_welcome_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_redis_connection(self):
+        redis = Redis(host="redis-server", db=0)
+        self.app.get("/visit")
+        self.assertEqual(int(redis.get("counter")), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
