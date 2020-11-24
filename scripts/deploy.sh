dc8b25da86140e4c4117f2bcaecb8cb99c13d3a6
Luc Bertin <lucbertin@outlook.com>
2020-11-24 10:31:15 +0100

    solving the -d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ scp -r ./app/* administrateur@devincidocker01003.northeurope.cloudapp.azure.com:
 # copy the docker-compose file to the remote
 scp docker-compose.yml administrateur@devincidocker01003.northeurope.cloudapp.azure.com:~/
 # docker-compose up on the remote 
-ssh -p 22 administrateur@devincidocker01003.northeurope.cloudapp.azure.com 'sudo docker-compose up --build'
+ssh -p 22 administrateur@devincidocker01003.northeurope.cloudapp.azure.com 'sudo docker-compose up --build -d'
